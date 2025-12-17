dc452898e8416025dfef3d1e8fdf2245cc3f917c
anthonydivine555 <divineanthony008@gmail.com>
2025-12-17 04:22:32 -0800

    first commit

---
+++ script/script.js
@@ -0,0 +1,54 @@
+const menuElement = document.getElementById('menu')
+const closeElement = document.getElementById('close')
+const sideMenu = document.querySelector('.menu-bar')
+
+menuElement.addEventListener('click', () => {
+  sideMenu.style.left = '0'
+  closeElement.style.display = 'block'
+  console.log('come here')
+})
+closeElement.addEventListener('click', () => {
+  sideMenu.style.left = '-100%';
+  closeElement.style.display = 'none';
+
+})
+
+const menuList = document.querySelectorAll('.items')
+
+console.log(menuList[0])
+menuList.forEach(list => {
+  list.addEventListener('click', () => {
+    menuList.forEach(item => {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active')
+        item.classList.add('items')
+      }
+    })
+
+  list.classList.add('active')
+
+  sideMenu.style.left = '-100%';
+  closeElement.style.display = 'none';
+  })
+})
+
+window.addEventListener("load", scrollEffect)
+window.addEventListener("scroll", scrollEffect)
+function scrollEffect () {
+  menuList.forEach(item => {
+    console.log(window.pageYOffset)
+    item.classList.remove('active')
+    if(window.pageYOffset>=0 && window.pageYOffset < 550 && item.textContent.trim() === 'home') {
+        item.classList.add('active')
+      } else if(window.pageYOffset>=550 && window.pageYOffset < 1024 && item.textContent.trim() === 'about') {
+        item.classList.add('active')
+      } else if(window.pageYOffset>=1024 && window.pageYOffset < 2141 && item.textContent.trim() === 'project') {
+        item.classList.add('active')
+      } else if(window.pageYOffset>=2141 && window.pageYOffset < 2800 && item.textContent.trim() === 'skills') {
+        item.classList.add('active')
+      } else if(window.pageYOffset>=2800 &&  window.pageYOffset < 3900 && item.textContent.trim() === 'contact') {
+        item.classList.add('active')
+      }
+  })
+}
+  
